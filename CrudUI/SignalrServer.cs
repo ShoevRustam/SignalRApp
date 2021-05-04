8d0c841bb1f8e115c2509a11b35fb71a208c1ede
RustamShoev <Shoev_97@list.ru>
2021-05-04 13:19:38 +0300

    CreateProject

---
+++ CrudUI/SignalrServer.cs
@@ -0,0 +1,14 @@
+﻿using CrudUI.Models;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudUI
+{
+    public class SignalrServer:Hub
+    {
+
+    }
+}
